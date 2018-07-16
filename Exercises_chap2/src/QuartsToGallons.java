b2bed6a2cef0da0fb1ffee815734c3ce23de0859
Sherin24 <sherinsarah.varghese@my.jcu.edu.au>
2018-07-16 14:39:56 +0800

    Other Exercises in week 2 prac 2

---
+++ Exercises_chap2/src/QuartsToGallons.java
@@ -0,0 +1,9 @@
+public class QuartsToGallons
+{
+    public static void main(String[] args)
+    {
+        int quarts = 4;
+        int quartsForJob = 20;
+        System.out.println("A job that needs " + quartsForJob + " quarts requires 4 gallons plus " + quarts + " quarts");
+    }
+}
