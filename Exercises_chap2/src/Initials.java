c672fa183c72e3d6442d8570c0928b0c44e5403b
Sherin24 <sherinsarah.varghese@my.jcu.edu.au>
2018-07-21 18:23:13 +0800

    Other Exercises in week 2 prac 2

---
+++ Exercises_chap2/src/Initials.java
@@ -0,0 +1,15 @@
+public class Initials
+{
+    public static void main(String[] args)
+    {
+        //char first = 'S';
+        //char second = 'S';
+        //char third = 'V';
+        //System.out.print(first + "." + second + "." + third + ".");
+        String name = "SSV";
+        for(int i = 0; i < name.length(); i++)
+        {
+            System.out.print(name.charAt(i) + ".");
+        }
+    }
+}
