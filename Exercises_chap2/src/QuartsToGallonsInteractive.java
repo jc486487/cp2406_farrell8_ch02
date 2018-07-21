be3c0abe2f5a0042d7e8bd3030fc84857554633c
Sherin24 <sherinsarah.varghese@my.jcu.edu.au>
2018-07-21 17:50:59 +0800

    Other Exercises in week 2 prac 2

--- Exercises_chap2/src/QuartsToGallonsInteractive.java
+++ Exercises_chap2/src/QuartsToGallonsInteractive.java
@@ -1,9 +1,12 @@
-public class QuartsToGallons
+import java.util.Scanner;
+public class QuartsToGallonsInteractive
 {
     public static void main(String[] args)
     {
+        Scanner inputDevice = new Scanner(System.in);
         int QUARTS = 4;
-        int quartsForJob = 18;
+        System.out.print("Enter the number of quarts required for the job >> ");
+        int quartsForJob = inputDevice.nextInt();
         int gallons = quartsForJob/QUARTS;
         int leftQuarts = quartsForJob%QUARTS;
         System.out.println("A job that needs " + quartsForJob + " quarts requires " + gallons + " gallons plus " + leftQuarts + " quarts");
